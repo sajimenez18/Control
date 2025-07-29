41df4495b230168ad97f706d652bea5debc4272d
Stephs <sajimenez@scl.edu.gt>
2025-07-29 16:24:31 -0600

    Diseño item

--- modulos/item/itemModulo.js
+++ modulos/item/itemModulo.js
@@ -3,5 +3,19 @@ export function item(){
     let item = document.createElement('div');
     item.className = "item";
 
+    let titulo = document.createElement('h1');
+    titulo.innerText = "Snoopy";
+
+    let img = document.createElement('img');
+    img.src = '../../assets/snoopy.png';
+    img.className = "snoopy";
+
+    let descripcion = document.createElement('p');
+    descripcion.innerText = "Encontraste a Snoopy fan de la música; buenos gustos músicales";
+    descripcion.className = "descrip";
+    
+    item.appendChild(titulo);
+    item.appendChild(img);
+    item.appendChild(descripcion);
     return item;
 }
